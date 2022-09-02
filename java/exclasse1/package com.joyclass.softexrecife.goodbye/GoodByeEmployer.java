a58176427f6232d150441e2db93eb4ff686c442e
Savio Menezes <saviomenezesbrito@gmail.com>
2022-09-02 10:45:34 -0300

    Java

---
+++ java/exclasse1/package com.joyclass.softexrecife.goodbye/GoodByeEmployer.java
@@ -0,0 +1,11 @@
+package com.joyclass.softexrecife.goodbye;
+
+public class GoodByeEmployer {
+  
+  public static void main(String[] args)
+  {
+      
+      System.out.println(
+          "
+          It was a pleasure to welcome you to SoftEx employee Sávio. Goodbye");
+  }
---
+++ java/exclasse1/package com.joyclass.softexrecife.welcome/WelcomeEmployer.java
@@ -0,0 +1,10 @@
+package com.joyclass.softexrecife.welcome;
+
+public class WelcomeEmployer {
+  
+  public static void main(String[] args)
+  {
+      
+      System.out.println(
+          "welcome to the company SoftEx employee Sávio");
+  }
